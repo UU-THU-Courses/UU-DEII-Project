31690079282fd18c87b365ce209e3e05902a2d8b
Usama Zafar <usama-zafar@outlook.com>
2024-05-14 19:54:47 +0200

    Adding keypair generation. Closes: Missing dynamic keypairs using OpenSSH.  #2

--- deployment/deploy.py
+++ deployment/deploy.py
@@ -1,28 +1,36 @@
 import os
 import fire
 import random
-import requests, time
+import subprocess
 
 from utils.requisition import create_instance
 from utils.configs import parse_configs, write_configs
+#from utils.keygen import generate_keypair
 
 MAX_ATTEMPTS = 10
 
-def deploy_headnode(name_prefix, configs):
+def deploy_headnode(name_prefix, configs, ssh_key):
     """A module to create the headnode and send config files."""
-    
+
+    cloud_cfg = parse_configs(config_path=configs["instance_configs"])
+    configs["instance_configs"] = "__temp_dir__/temp_headnode_cfg.yaml"
+    cloud_cfg["users"]["ssh_authorized_keys"].append(ssh_key)
+    os.makedirs("__temp_dir__", exist_ok=True)
+    write_configs(configs["instance_configs"], cloud_cfg)
+
     # Create the headnode and obtain ip address
     ip_addr = create_instance(name=f"{name_prefix}-headnode", configs=configs)
 
     return ip_addr
 
-def launch_workernodes(name_prefix, num_nodes, head_ip, configs):
+def launch_workernodes(name_prefix, num_nodes, head_ip, configs, ssh_key):
     # Update cloud configurations in order to write
     # the head ip address to a temporary file at
     # worker node
     
     cloud_cfg = parse_configs(config_path=configs["instance_configs"])
-    configs["instance_configs"] = "__temp_dir__/temp_cloud_cfg.yaml"
+    configs["instance_configs"] = "__temp_dir__/temp_worknode_cfg.yaml"
+    cloud_cfg["users"]["ssh_authorized_keys"].append(ssh_key)
     cloud_cfg["write_files"] = [{
         "content": f"{head_ip}",
         "path": "/HEAD-IP.txt",
@@ -72,16 +80,24 @@ def full_deployment(config_file = "configs/deploy-cfg.yaml"):
     # Produce a random identifier
     identifier = random.randint(1000,9999)
 
+    # Generate a new private/public keypair
+    os.makedirs("temp_keypairs", exist_ok=True)
+    # generate_keypair(keypath="temp_keypairs")
+    run_cmd = f'ssh-keygen -q -t rsa -N "" -f temp_keypairs/id_rsa'
+    subprocess.call(run_cmd, shell = True)
+    with open("temp_keypairs/id_rsa.pub", "r") as keyfile:
+        ssh_key = keyfile.read().strip()
+
     # Perform deployment of headnode
     # rest all will be handled by headnode
     print("Deploying head node ... ")
-    head_ip = deploy_headnode(name_prefix=f"UZ-{identifier}", configs = configs["instances"]["headnode"])
+    head_ip = deploy_headnode(name_prefix=f"UZ-{identifier}", configs=configs["instances"]["headnode"], ssh_key=ssh_key)
     print(f"Head node deployed at {head_ip} ...")
     
     # Obtain the swarm token
     # docker swarm join-token manager -q
     print("\nDeploying worker nodes ... ")
-    worker_ips = launch_workernodes(name_prefix=f"UZ-{identifier}", num_nodes=configs["instances"]["workernodes"]["numworkers"], head_ip=head_ip, configs=configs["instances"]["workernodes"]["workercfgs"])
+    worker_ips = launch_workernodes(name_prefix=f"UZ-{identifier}", num_nodes=configs["instances"]["workernodes"]["numworkers"], head_ip=head_ip, configs=configs["instances"]["workernodes"]["workercfgs"], ssh_key=ssh_key)
 
 
 if __name__ == "__main__":
--- deployment/utils/keygen.py
+++ deployment/utils/keygen.py
@@ -1,38 +1,24 @@
 from os import chmod
 from OpenSSL import crypto
+import subprocess
 
-def generate_keypair(private_path, public_path):
+def generate_keypair(keypath, bits = 3072):
     """A cryptographic module to generate RSA keypair."""
-
-    # Generate an 2048 bit RSA private key
-    # and write the private key to a file
-    # key = RSA.generate(2048)
-    # with open(private_path, "wb") as keyfile:
-    #     chmod(private_path, 0o600)
-    #     keyfile.write(key.exportKey("PEM"))
-    
-    # Generate the public key corresponding
-    # to private key and write key to a file
-    # pubkey = key.publickey()
-    # with open(public_path, "wb") as keyfile:
-    #     keyfile.writable(pubkey.exportKey("OpenSSH"))
-
     # Use OpenSSL wrapper to generate the keys
     # Generate an 3072 bit RSA private key
     private_key = crypto.PKey()
     private_key.generate_key(crypto.TYPE_RSA, 3072)
-    ## ssh-keygen -q -t rsa -N "" -f "./t1_rsa"
     
     # Write the private key to a file
-    with open(private_path, "wb") as keyfile:
+    private_keypath = keypath+"/privatekey"
+    with open(private_keypath, "wb") as keyfile:
         # chmod(private_path, 0o400)
         keyfile.write(crypto.dump_privatekey(crypto.FILETYPE_PEM, private_key))
     
-    # Generate the public key corresponding
-    # to private key and write key to a file
-    with open(public_path, "wb") as keyfile:
-        keyfile.write(crypto.dump_publickey(crypto.FILETYPE_PEM, private_key))
-
+    # Generate equivalent public key
+    # run_cmd = f'ssh-keygen -q -t rsa -N "" -f {private_keypath}'
+    # subprocess.call(run_cmd, shell = True)
 
 if __name__ == "__main__":
-    generate_keypair("__temp/pr_key.pem", "__temp/pb_key")
+    # os.makedirs("temp_keypairs", exist_ok=True)
+    generate_keypair("__temp__")
