1cfbc8117889377c5d1f75d39defa6c7dd954607
Usama Zafar <usama-zafar@outlook.com>
2024-05-17 18:13:49 +0200

    Changing instance flavor to xlarge and xlarge.highcpu

--- unittester/discovery/service_scale.sh
+++ unittester/discovery/service_scale.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+add_count=8
+[ ! -z "$1" ] && add_count="$1"
+
 # Fetch current replica count
 # for worker containers
 replicas=$(docker service inspect --format '{{.Spec.Mode.Replicated.Replicas}}' unittester_worker)
 
 # Update the replica count
-((replicas+=5))
+((replicas+=add_count))
 
 # Scale the worker service
 docker service scale unittester_worker=${replicas} -d
