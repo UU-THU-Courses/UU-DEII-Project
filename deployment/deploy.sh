a8b4b2e50cadb9630698098f180b5c1a52828ad9
Usama Zafar <usama-zafar@outlook.com>
2024-05-22 09:32:47 +0200

    splitting deployment and setup logic into two different scripts

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,38 +1,7 @@
 #!/bin/bash
 
-# Check for root access
-if [[ $EUID -ne 0 ]];
-then
-    # Rerun the script with elevated priviledge
-    exec sudo /bin/bash "$0" "$@";
-
-    exit "$?";
-fi
-
 config_file="configs/deploy-cfg.yaml"
 [ ! -z "$1" ] && config_file="$1"
-
-# Perform updates and upgrade
-apt update; apt -y upgrade
-
-# Install required tools and dependencies
-apt -y install curl tmux 
-
-# Install pip3 package manager and virtual environment
-apt -y install python3 python3-pip python3-venv
-
-# Install Openstack components
-add-apt-repository cloud-archive:yoga
-apt -y install nova-compute
-
-# Install API related packages
-apt -y install python3-openstackclient python3-novaclient python3-keystoneclient
-
-# Install dependencies
-python3 -m pip install -r requirements.txt
-
-# Perform updates and upgrade
-apt update; apt -y upgrade
 
 # Start deployment
 python3 deploy.py --full --config_file=${config_file}
