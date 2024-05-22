de7aec49ab079e828b6d6e0fcb41b775d88b0d28
Usama Zafar <usama-zafar@outlook.com>
2024-05-22 15:16:57 +0200

    finalizing node drain, service scaledown, and server delete.

--- unittester/manager/scripts/drain.sh
+++ unittester/manager/scripts/drain.sh
@@ -7,21 +7,26 @@ log_filename="logfile.txt"
 
 worker_nodes=$(docker node inspect --format '{{.Description.Hostname}}' $(docker node ls --filter role="worker" -q))
 worker_count=$(docker node inspect --format '{{.Description.Hostname}}' $(docker node ls --filter role="worker" -q) | wc -l)
+n_containers=$(docker service inspect --format '{{.Spec.Mode.Replicated.Replicas}}' unittester_worker)
 
-printf "Worker nodes: ${worker_count}\n"
+# Compute the scale downed containers
+((n_containers-=8*worker_count))
+
+printf "# of worker nodes: ${worker_count}\n"
 
 # Convert the docker response to an array
 readarray -t  worker_nodes <<<"${worker_nodes}"
 
 if [ "${drain_count} " -le "${worker_count}" ]; then
-        for ((idx=${#worker_nodes[@]}-1,count=drain_count; idx>=0 && count>0; idx--, count--)) ; do
-            printf "Draining: ${worker_nodes[idx]}\n"
-            # Set the node availability to drain
-            # docker node update --availability drain ${worker_nodes[idx]}
-            # Scale down the number of worker containers
-            # Log the node name to a file
-            printf "${worker_nodes[idx]}\n" >> ${log_filename}
-        done
+    for ((idx=${#worker_nodes[@]}-1,count=drain_count; idx>=0 && count>0; idx--, count--)) ; do
+        printf "Draining: ${worker_nodes[idx]}\n"
+        # Set the node availability to drain
+        docker node update --availability drain ${worker_nodes[idx]}
+        # Scale down the number of worker containers
+        docker service scale unittester_worker=${n_containers} -d
+        # Log the node name to a file
+        printf "${worker_nodes[idx]}\n" >> ${log_filename}
+    done
 else
-        printf "Error: Drain count ${drain_count} cannot be greater than active workers ${worker_count}.\n"
+    printf "Error: Drain count ${drain_count} cannot be greater than active workers ${worker_count}.\n"
 fi
