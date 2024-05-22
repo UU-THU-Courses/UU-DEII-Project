abc302f14aa04438d0205a36a51d5f814300b9cb
Usama Zafar <usama-zafar@outlook.com>
2024-05-22 16:08:36 +0200

    scaling service before draining node.

--- unittester/manager/scripts/drain.sh
+++ unittester/manager/scripts/drain.sh
@@ -15,20 +15,25 @@ printf "# of worker nodes: ${worker_count}\n"
 readarray -t  worker_nodes <<<"${worker_nodes}"
 
 if [ "${drain_count} " -le "${worker_count}" ]; then
-    for ((idx=${#worker_nodes[@]}-1,count=drain_count; idx>=0 && count>0; idx--, count--)) ; do
-        printf "Draining: ${worker_nodes[idx]}\n"
+    # Scale down first before doing anything else
+    for ((idx=${#worker_nodes[@]}-1,count=drain_count; idx>=0 && count>0; idx--,count--)) ; do
         # Check the number of containers running on current node
         container_count=$(docker node ps --format "{{.ID}}" --filter "desired-state=running" ${worker_nodes[idx]} | wc -l)
         # Update the n_containers that should be running after
         # the node has been drained
         ((n_containers-=container_count))
+    done
+    
+    # Scale down the number of worker containers
+    docker service scale unittester_worker=${n_containers} -d
+
+    for ((idx=${#worker_nodes[@]}-1,count=drain_count; idx>=0 && count>0; idx--,count--)) ; do
+        printf "Draining: ${worker_nodes[idx]}\n"
         # Set the node availability to drain
         docker node update --availability drain ${worker_nodes[idx]}
         # Log the node name to a file
         printf "${worker_nodes[idx]}\n" >> ${log_filename}
     done
-    # Scale down the number of worker containers
-    docker service scale unittester_worker=${n_containers} -d
 else
     printf "Error: Drain count ${drain_count} cannot be greater than active workers ${worker_count}.\n"
 fi
